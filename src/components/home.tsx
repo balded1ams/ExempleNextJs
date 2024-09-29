a4e6456f560241dc60b13c04c669745b4f6994cf
chlorate <barthesc@etu.univ-grenoble-alpes.fr>
2024-09-30 00:24:58 +0200

    fix

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -27,18 +27,16 @@ const Home: React.FC = () => {
     };
 
     return (
-        <div className="bg-slate-900">
+        <div className="min-h-full w-screen bg-slate-900">
             <Navbar/>
-
-            {/*pt pour la navbar*/}
-            <div className="px-4 flex flex-col lg:flex-row h-full pt-16">
-                <div className="w-full lg:w-1/2 overflow-visible mb-4 lg:mb-0">
+            <div className="px-4 flex flex-col lg:flex-row">
+                <div className="w-full lg:w-1/2 h-auto overflow-hidden lg:mb-0">
                     <ThreeScene />
                 </div>
-                <div className="w-full lg:w-1/2 border overflow-x-visible flex flex-col lg:flex-row">
-                    <div className="w-full xl:w-3/4 px-2 mt-4">
-                        <Card isBlurred className="border rounded bg-background/100 w-full mb-4 card-shadow">
-                            <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
+                <div className="w-full lg:w-1/2 border overflow-hidden flex flex-col lg:flex-row">
+                    <div className="w-full xl:w-3/4 px-2">
+                        <Card className="border rounded bg-background/100 card-shadow max-w-full">
+                        <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
                                 <h1 className="font-bold text-lg">\NaClO4</h1>
                                 <div className="flex gap-1 pb-1.5 justify-between">
                                     <Minus size={16}/>
@@ -52,7 +50,7 @@ const Home: React.FC = () => {
                                 </h1>
                             </CardBody>
                         </Card>
-                        <Card isBlurred className="border rounded bg-background/100 card-shadow">
+                        <Card isBlurred className="border rounded bg-background/100 max-w-full card-shadow">
                             <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
                                 <h1 className="font-bold text-lg">\parcour <Videotape/> </h1>
                                 <div className="flex gap-1 pb-1.5 justify-between">
@@ -66,10 +64,10 @@ const Home: React.FC = () => {
                             </CardBody>
                         </Card>
                         <div>
-                            <Card isBlurred className="border rounded bg-background/100 vhs-effect card-shadow"
+                            <Card isBlurred className="border rounded bg-background/100 max-w-full vhs-effect card-shadow"
                                   onMouseEnter={addVHSEffect}
                                   onMouseLeave={removeVHSEffect}>
-                                <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
+                                <CardHeader className="pb-0 px-4 justify-between border-b">
                                     <h1 className="font-bold text-large">\cat </h1>
                                     <div className="flex gap-1 pb-1.5 justify-between">
                                         <Minus size={16}/>
@@ -88,9 +86,8 @@ const Home: React.FC = () => {
                             </Card>
                         </div>
 
-                        {/* Bouton pour scroller vers la section du parcours */}
                         <button
-                            className="mt-8 text-white border p-2 rounded"
+                            className="text-white border p-2 rounded"
                             onClick={scrollToSection}
                         >
                             Voir mon parcours
@@ -99,7 +96,7 @@ const Home: React.FC = () => {
                 </div>
             </div>
 
-            <div id="my-journey-section" className="px-4 py-8 bg-slate-900">
+            <div id="my-journey-section" className="px-4 bg-slate-900">
                 <h2 className="text-3xl font-bold text-white mb-4">Mon Parcours</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <Card className="border rounded bg-background/100 card-shadow">
