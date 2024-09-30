818f007d4469c1c6e7858dd0b5963f3708b355e3
NaClO4 <barthesc@etu.univ-grenoble-alpes.fr>
2024-09-30 14:41:25 +0200

    new shadow

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -28,12 +28,11 @@ const Home: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen bg-slate-900">
             <Navbar />
-            <div className="px-4 pt-16 flex flex-col lg:flex-row">
+            <div className="px-4 flex flex-col lg:flex-row">
                 <div className="w-full lg:w-1/2 h-auto overflow-hidden lg:mb-0">
                     <ThreeScene/>
                 </div>
-                <div className="w-full lg:w-1/2 flex flex-col gap-4">
-                    {/* Première rangée : NaClO4 et Chat */}
+                <div className="w-full lg:w-1/2 flex flex-col justify-center gap-4">
                     <div className="flex w-full">
                         <div className="w-3/4 px-2">
                             {/* Card NaClO4 */}
@@ -54,39 +53,6 @@ const Home: React.FC = () => {
                             </Card>
                         </div>
                         <div className="w-1/4 px-2">
-                            {/* Card Chat */}
-                            <Card className="border rounded bg-background/100 card-shadow">
-                                <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
-                                    <h1 className="font-bold text-lg">\Cat</h1>
-                                </CardHeader>
-                                <CardBody className="flex justify-center items-center py-2">
-                    <pre className="font-mono whitespace-pre text-sm">
-                         ╱|、{'\n'}
-                        (˚ˎ。7{'\n'}
-                        |、˜〵{'\n'}
-                        じしˍ,)ノ
-                    </pre>
-                                </CardBody>
-                            </Card>
-                        </div>
-                    </div>
-
-                    {/* Deuxième rangée : Parcour et Autre Carte */}
-                    <div className="flex w-full">
-                        <div className="w-1/2 px-2">
-                            {/* Autre Carte */}
-                            <Card
-                                className="border rounded bg-background/100 max-w-full card-shadow ">
-                                <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
-                                    <h1 className="font-bold text-lg">Autre Carte</h1>
-                                </CardHeader>
-                                <CardBody className="flex justify-center items-center py-2">
-                                    {/* Content */}
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="w-1/2 px-2">
-                            {/* Card Parcour */}
                             <Card isBlurred
                                   className="border rounded bg-background/100 max-w-full card-shadow ">
                                 <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
@@ -94,25 +60,51 @@ const Home: React.FC = () => {
                                         <h1 className="font-bold text-lg mr-2">\parcour</h1>
                                         <Videotape/>
                                     </div>
-                                    <div className="flex gap-1 pb-1.5 justify-between">
-                                        <Minus size={16}/>
-                                        <Square size={16}/>
-                                        <X size={16}/>
-                                    </div>
                                 </CardHeader>
-                                <CardBody className="flex justify-center items-center py-2">
+                                <CardBody className="flex justify-center items-center py-4">
                                     <button
-                                        className="text-white border p-2 rounded"
+                                        className="text-white"
                                         onClick={scrollToSection}
                                     >
-                                        Voir mon parcours
+                                        mon parcours
                                     </button>
                                 </CardBody>
                             </Card>
                         </div>
                     </div>
-                </div>
 
+                    {/* Deuxième rangée : Parcour et Autre Carte */}
+                    <div className="flex w-full">
+                        <div className="w-1/4 px-2">
+                            {/* Card Chat */}
+                            <Card className="border rounded bg-background/100 card-shadow">
+                                <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
+                                    <h1 className="font-bold text-lg">\Cat</h1>
+                                </CardHeader>
+                                <CardBody className="flex justify-center items-center py-2">
+                                    <pre className="font-mono whitespace-pre text-sm">
+                                         ╱|、{'\n'}
+                                        (˚ˎ。7{'\n'}
+                                        |、˜〵{'\n'}
+                                        じしˍ,)ノ
+                                    </pre>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="w-3/4 px-2">
+                            {/* Autre Carte */}
+                            <Card className="border rounded bg-background/100 max-w-full card-shadow ">
+                                <CardHeader className="pb-0 pt-2 px-4 justify-between border-b">
+                                    <h1 className="font-bold text-lg">\me</h1>
+                                </CardHeader>
+                                <CardBody className="flex justify-center items-center py-3">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut jzfjsdbfjkbmlkqdv df</p>
+                                </CardBody>
+                            </Card>
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
             <div id="my-journey-section" className="px-4 bg-slate-900">
